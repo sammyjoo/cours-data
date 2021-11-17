4cda4394de1e26154c45ff05c73f74eb1df7f959
sammyjoo <91895424+sammyjoo@users.noreply.github.com>
2021-11-17 15:59:00 +0100

    Add files via upload

---
+++ ecommerce/style.js
@@ -0,0 +1,13 @@
+
+const produits = [
+    {id : 1, titre : "Iphone 13", couleur : "rouge"},
+    {id : 2, titre : "Iphone 16", couleur : "bleu"},
+    {id : 3, titre : "Samsung 13", couleur : "rouge"}
+]
+
+const rougesProducts = produits.filter(produit => produit.couleur === "rouge" )
+
+ 
+for (const element of tabTest) {
+  console.log(tabTest);
+}
